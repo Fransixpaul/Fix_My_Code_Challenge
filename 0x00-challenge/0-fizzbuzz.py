2e41d449024da3ca5ac6fe294c4a330a1adae2e3
francis <kayininse@gmail.com>
2024-05-31 11:42:11 +0100

    Fix the code FizzBuzz

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -35,14 +35,9 @@ if __name__ == '__main__':
         print("Example: ./0-fizzbuzz.py 89")
         sys.exit(1)
 
-    number = int(sys.argv[1])
-    fizzbuzz(number)
-
     try:
-        number = int(sys.argv[])
+        number = int(sys.argv[1])
+        fizzbuzz(number)
     except ValueError:
         print("Invalid number")
         sys.exit(1)
-
-
-    fizzbuzz(number)
